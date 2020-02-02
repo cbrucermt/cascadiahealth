588337d402a3bb011a62da49734a84e8d604e586
May Pongpitpitak <me@mayppong.com>
2020-02-02 12:05:27 -0800

    Remove the need for jQuery UI easing component

--- js/creative.js
+++ js/creative.js
@@ -7,12 +7,11 @@
 (function($) {
     "use strict"; // Start of use strict
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
+        }, 1250);
         event.preventDefault();
     });
 
